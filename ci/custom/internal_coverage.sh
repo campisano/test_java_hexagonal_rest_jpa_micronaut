f57a7c251d9f1605d124230d32b35e56cb1859bb
campisano <riccardo.campisano@gmail.com>
2020-03-29 19:36:26 -0300

    fix:        fix jacoco strange problem maybe with surefire plugin?
    https://stackoverflow.com/a/49654952

--- ci/custom/internal_coverage.sh
+++ ci/custom/internal_coverage.sh
@@ -8,4 +8,4 @@ apt-get -qq -y update
 apt-get -qq -y install curl git > /dev/null
 apt-get -qq -y clean
 
-bash <(curl -s https://codecov.io/bash) -t "${TOKEN}"
+bash <(curl -s https://codecov.io/bash) -X coveragepy -X xcode -s ./target -t "${TOKEN}"
